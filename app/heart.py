f5cdcc968348b81ec9aab225a563e5fafdbb3823
Mayoza, Rabbi <32793615+Pollycarpus@users.noreply.github.com>
2019-01-18 23:51:40 +0700

    Add files via upload

---
+++ app/heart.py
@@ -0,0 +1,71 @@
+from flask import Flask, render_template, request, jsonify
+import pickle
+import pandas as pd
+import numpy as np
+
+from sklearn.preprocessing import StandardScaler
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/result', methods = ['POST'])
+def run():
+    # Get data input
+    age = request.form['age']
+    sex = request.form['sex']
+    chestpain = request.form['chestpain_type']
+    rest_blood = request.form['resting_blood_pressure']
+    serum_chol = request.form['serum_cholesterol']
+    fast_blood = 1 if int(request.form['sugar']) > 120 else 0
+    rest_ecg = request.form['resting']
+    max_heart_rate = request.form['max_heart_rate']
+    exercise = request.form['include_angina']
+    depression = request.form['depression']
+    peak = request.form['peak_exercise']
+
+    # Load Model
+    with open('model.sav', 'rb') as file :
+        model = pickle.load(file)
+
+    # Normalize Data
+    rest_blood = ((float(rest_blood)*2)-200)/200
+    serum_chol = ((float(serum_chol)*2)-529)/529
+    max_heart_rate = ((float(max_heart_rate)*2)-202-60)/(202-60)
+    depression = ((float(depression)*2)-62+2.6)/(62+2.6)
+
+    # Create data
+    if (rest_ecg == 0) :
+        if (peak == 1) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,1,0,0,1,0,0]])
+        elif (peak == 2) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,1,0,0,0,1,0]])
+        else :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,1,0,0,0,0,1]])
+    elif (rest_ecg == 1) :
+        if (peak == 1) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,1,0,1,0,0]])
+        elif (peak == 2) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,1,0,0,1,0]])
+        else :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,1,0,0,0,1]])
+    else :
+        if (peak == 1) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,0,1,1,0,0]])
+        elif (peak == 2) :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,0,1,0,1,0]])
+        else :
+            data = np.array([[int(age),int(sex),int(chestpain),rest_blood,serum_chol,float(fast_blood),max_heart_rate,float(exercise),depression,0,0,1,0,0,1]])
+
+    # Predict
+    predict = model.predict(data)
+
+    print("------------------------------")
+    print(predict[0])
+
+    return jsonify(int(predict[0]))
+
+if __name__ == '__main__' :
+    app.run(debug = True)
